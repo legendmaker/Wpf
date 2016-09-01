9fd3c3a6c179b0be37dc847d9b6bd6ed7eb8f7dc
Richard Zhang <richardzhang@apintec.com>
2016-09-01 14:50:05 +0800

    Add image test.

---
+++ Study/Study/testImage/ImageSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace testImage
+{
+    public class ImageSource : DependencyObject
+    {
+        public BitmapImage Image
+        {
+            get { return (BitmapImage)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(BitmapImage), typeof(ImageSource), new PropertyMetadata(null));
+        public ImageSource()
+        {
+
+        }
+    }
+}
---
+++ Study/Study/testImage/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace testImage
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //image.Source = new BitmapImage(new Uri(@"C:\Users\Richard\Pictures\3.bmp"));
+            using(MemoryStream ms =new MemoryStream())
+            {
+                Bitmap bitmap = new Bitmap(@"C:\Users\Richard\Pictures\2.png");
+                bitmap.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = ms;
+                bi.EndInit();
+                bi.Freeze();
+                image.Source = bi;
+            }
+        }
+
+    }
+}
