c7ebda75db68fc06d58b50a5adced6e11418ca78
Richard Zhang <richardzhang@apintec.com>
2016-09-14 10:52:59 +0800

    Add.

---
+++ Study/Study/Clock/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer = new DispatcherTimer();
+        public string CurrentTime
+        {
+            get { return (string)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentTime.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(string), typeof(MainWindow), new PropertyMetadata(null));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textblockTime.DataContext = this;
+            _timer.Interval = new TimeSpan(1);
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString();
+        }
+    }
+}
